d9fd598b4b87b0729dcab2d614265cd55fae55f8
Guilherme Dantas <guidanoli@proton.me>
2025-03-28 16:14:25 -0300

    Add test-project-foo

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {IERC20Portal} from "test-project-foo-0.0.2/src/portals/IERC20Portal.sol";
+
 contract Counter {
     uint256 public number;
 
